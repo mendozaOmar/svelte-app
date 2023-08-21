3e4a927db70a4746e2b4c2d5c8aaf4acc9f51865
Omar Mendoza <mendoza8133@gmail.com>
2023-08-21 07:00:40 -0600

    feat: modified conter

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+  export let title = "Default Title";
   let count: number = 0
+  $: double = count * 2;
   const increment = () => {
     count += 1
   }
 </script>
 
+<div id="title" >
+  <h2>{title}</h2>
+</div>
+
 <button on:click={increment}>
-  count is {count}
+  count is {count} and double is {double}
 </button>
